c6abf43339ee73077c178c68582932612069a7cb
Mathias231 <mathias.krogh@outlook.com>
2023-05-30 12:46:00 +0200

    Created callbacks to add admin role and userId to session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth/next';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/lib/db';
 
 if (!process.env.GITHUB_ID) throw new Error('GITHUB_ID Missing');
 if (!process.env.GITHUB_SECRET) throw new Error('GITHUB_ID Missing');
@@ -11,4 +14,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+
+  // Adding admin & userId to session
+  callbacks: {
+    // Updating session on client with columns from database. Right now its only "WhiteListed" (true or false)
+    async session({ session }) {
+      // Finds user by email
+      const userData = await prisma.user.findFirst({
+        where: {
+          email: session.user?.email,
+        },
+      });
+      // Updates session by adding whiteListed - boolean
+      const updatedSession = {
+        ...session,
+        user: {
+          ...session.user,
+          admin: userData?.admin,
+          userId: userData?.id,
+        },
+      };
+
+      // DefaultSession (just in case)
+      const defaultSession = {
+        user: {
+          name: '',
+          email: '',
+          image: '',
+        },
+        expires: '',
+      };
+      // Sets result as updatedSession. Sets defaultSession if something goes wrong
+      const result = updatedSession || defaultSession;
+      return result;
+    },
+  },
 });
