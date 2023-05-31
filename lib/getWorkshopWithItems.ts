43ad5dce88a8409616c0ca085d32cdd4e9e814f9
Mathias231 <mathias.krogh@outlook.com>
2023-05-31 18:18:58 +0200

    Created SWR Hook

---
+++ lib/getWorkshopWithItems.ts
@@ -0,0 +1,21 @@
+import { IWorkshop } from '@/types/workshop.types';
+import axios from 'axios';
+import useSWR from 'swr';
+
+const fetcher = async <T>(url: string) => {
+  let res = await axios.request<T>({
+    url: url,
+    method: 'get',
+  });
+
+  return res.data;
+};
+
+export default function GetWorkshopWithItems() {
+  const { data, error, isLoading, mutate } = useSWR(
+    `/api/workshop?limit=10`,
+    () => fetcher<IWorkshop>('/api/workshop'),
+  );
+
+  return { workshop: data, error, isLoading, mutate };
+}
