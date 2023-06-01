8876c8748bfb562bd87b21cc71a0ca4245b83e69
Mathias231 <mathias.krogh@outlook.com>
2023-06-01 15:38:40 +0200

    Link to createItem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
+import Item from '@/components/paginationItem/item';
+import GetItems from '@/lib/getItems';
 import GetWorkshopWithItems from '@/lib/getWorkshopWithItems';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { FaPlus } from 'react-icons/fa';
 
 export default function Home() {
-  const { workshop, error, isLoading, mutate } = GetWorkshopWithItems();
+  const { items, error, isLoading, nextPage, prevPage, mutate } = GetItems();
+  const { data: session } = useSession();
+  if (error) return <div>Error: {error}</div>;
+  if (isLoading) return <div>Laster data...</div>;
+  if (!items) return <div>Laster data...</div>;
 
-  if (!workshop) return <div>Verkstedet er tomt...</div>;
+  return (
+    <div>
+      {session?.user.role === 'ADMIN' && (
+        <div className="flex justify-center mt-3">
+          <Link href="/createItem/" title="Legg til verktøy">
+            <FaPlus size={40} className="hover:animate-spin" />
+          </Link>
+        </div>
+      )}
 
-  return <div></div>;
+      {/* {items.map((item) => {
+        return <Item key={item.id} {...item} />;
+      })}
+
+      <button
+        className="bg-gray-400 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-lg mt-4"
+        onClick={prevPage}
+      >
+        Tilbake
+      </button>
+
+      <div>----------</div>
+
+      <button
+        className="bg-gray-400 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-lg mt-4 ml-4"
+        onClick={nextPage}
+      >
+        Neste
+      </button> */}
+    </div>
+  );
 }
