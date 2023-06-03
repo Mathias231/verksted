f3e8f9ac76c29863046ebcb0d930e98c9bc9287d
Mathias231 <mathias.krogh@outlook.com>
2023-06-03 11:09:46 +0200

    Added security

--- pages/api/item/[itemId].ts
+++ pages/api/item/[itemId].ts
@@ -1,9 +1,63 @@
 import { prisma } from '@/lib/db';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
 import { createRouter } from 'next-connect';
 import z from 'zod';
+import { authOptions } from '../auth/[...nextauth]';
+import { NextApiRequestWithUser, getUser } from '@/middlewares/user';
 
-const router = createRouter<NextApiRequest, NextApiResponse>();
+const router = createRouter<NextApiRequestWithUser, NextApiResponse>();
+router.use(getUser);
+router.use(async (req, res, next) => {
+  // Gets itemId and user session
+  const itemId = z.string().parse(req.query.itemId);
+
+  // If user not logged in, return 401
+  if (!req.user) {
+    return res.status(401).send('Not logged in');
+  }
+
+  // If user is ADMIN, continue
+  if (req.user.user.role === 'ADMIN') {
+    return next();
+  }
+
+  // Tries to find a item that matches user's ID and itemID
+  const findItem = await prisma.items.findFirst({
+    where: {
+      AND: {
+        userId: req.user.user.userId,
+        id: itemId,
+      },
+    },
+  });
+
+  // If findItem is null, return 401
+  if (!findItem) {
+    return res.status(401).send('Not Authorized');
+  }
+
+  next();
+});
+
+router.put(async (req, res) => {
+  const itemId = z.string().parse(req.query.itemId);
+  const { itemType, dateOfPurchase, storageLocation } = req.body;
+
+  const updateItem = await prisma.items.update({
+    where: {
+      id: itemId,
+    },
+    data: {
+      itemType: itemType,
+      dateOfPurchase: dateOfPurchase,
+      storageLocation: storageLocation,
+      dateUpdated: new Date(),
+    },
+  });
+
+  res.status(200).send('Item Updated');
+});
 
 router.delete(async (req, res) => {
   const itemId = z.string().parse(req.query.itemId);
