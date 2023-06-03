215cd80c11d22b48738898d839a4398b0a86b776
Mathias231 <mathias.krogh@outlook.com>
2023-06-03 11:09:15 +0200

    Removed PUT Route and added security

--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -1,15 +1,24 @@
 import { prisma } from '@/lib/db';
+import { NextApiRequestWithUser, getUser } from '@/middlewares/user';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createRouter } from 'next-connect';
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
-router.post(async (req, res) => {
+// Finds user and puts it inside req object
+router.use(getUser);
+
+router.post(async (req: NextApiRequestWithUser, res) => {
   const { userId, itemId, content } = req.body;
+
+  if (!req.user) return res.status(401).send('Not logged in');
+
+  if (req.user.user.role !== 'ADMIN')
+    return res.status(401).send('Not Authorized');
 
   const createComment = await prisma.comments.create({
     data: {
-      userId: userId,
+      userId: req.user.user.userId,
       itemsId: itemId,
       content,
       dateCreated: new Date(),
@@ -18,19 +27,6 @@ router.post(async (req, res) => {
   });
 
   res.status(200).send('Comment Created!');
-});
-
-router.put(async (req, res) => {
-  const { commentId, content } = req.body;
-
-  const updateComment = await prisma.comments.update({
-    where: {
-      id: commentId,
-    },
-    data: { content: content, dateUpdated: new Date() },
-  });
-
-  res.status(200).send('Comment Updated!');
 });
 
 export default router.handler({
