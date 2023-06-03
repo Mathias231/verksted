063a98d29504d2a154452500d748d1ad904c09cd
Mathias231 <mathias.krogh@outlook.com>
2023-06-03 11:05:35 +0200

    Exporting NextAuth as an option for backend usage

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
 import { prisma } from '@/lib/db';
+import { AuthOptions } from 'next-auth';
 
 // Throw error if ID or SECRET is missing
 if (!process.env.GITHUB_ID) throw new Error('GITHUB_ID Missing');
@@ -11,7 +12,7 @@ if (!process.env.GOOGLE_CLIENT_ID) throw new Error('GOOGLE_CLIENT_ID Missing');
 if (!process.env.GOOGLE_CLIENT_SECRET)
   throw new Error('GOOGLE_CLIENT_SECRET Missing');
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
@@ -58,4 +59,5 @@ export default NextAuth({
       return result;
     },
   },
-});
+};
+export default NextAuth(authOptions);
