2bb9de1265dc989e2f9d9a095caa392116959d51
Mathias231 <mathias.krogh@outlook.com>
2023-06-06 08:11:20 +0200

    Displaying more item data & checks if user is logged in to see comment Icon

--- components/items/item.tsx
+++ components/items/item.tsx
@@ -39,25 +39,29 @@ function Item(item: IItem) {
       </div>
 
       <div className="p-1">
+        <p>Kategori: {item.category}</p>
         <p>Plassering: {item.storageLocation}</p>
         <p>Type: {item.itemType}</p>
+        <p>Dato Kjøpt: {item.dateOfPurchase}</p>
       </div>
 
-      <div className="flex space-x-3 p-1">
-        <Link href={`/itemPage/${item.id}`}>
-          <FaComment className="hover:text-green-500" />
-        </Link>
-        {session?.user.role === 'ADMIN' && (
-          <>
-            <button onClick={() => handleUpdate(item.id)}>
-              <FaPen className="hover:text-blue-600" />
-            </button>
-            <button onClick={() => handleDelete(item.id)}>
-              <FaTrash className="hover:text-red-600" />
-            </button>
-          </>
-        )}
-      </div>
+      {session?.user && (
+        <div className="flex space-x-3 p-1">
+          <Link href={`/itemPage/${item.id}`}>
+            <FaComment className="hover:text-green-500" />
+          </Link>
+          {session?.user.role === 'ADMIN' && (
+            <>
+              <button onClick={() => handleUpdate(item.id)}>
+                <FaPen className="hover:text-blue-600" />
+              </button>
+              <button onClick={() => handleDelete(item.id)}>
+                <FaTrash className="hover:text-red-600" />
+              </button>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
