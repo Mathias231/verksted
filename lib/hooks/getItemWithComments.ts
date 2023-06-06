0bf5f5ba4e0da7a3af4477e58d0bc9f938213cdc
Mathias231 <mathias.krogh@outlook.com>
2023-06-06 08:15:21 +0200

    Added mutate function to update data

--- lib/hooks/getItemWithComments.ts
+++ lib/hooks/getItemWithComments.ts
@@ -1,6 +1,7 @@
 import { IItemWithComments } from '@/types/workshop.types';
 import axios from 'axios';
 import useSWR from 'swr';
+import { useEffect } from 'react';
 
 const fetcher = async <T>(url: string) => {
   let res = await axios.request<T>({
@@ -12,11 +13,14 @@ const fetcher = async <T>(url: string) => {
 };
 
 export default function GetItemWithComments(itemId: string) {
-  const { data, error, isLoading, mutate } = useSWR(
-    `/api/item/comment/itemWithComment/${itemId}`,
-    () =>
-      fetcher<IItemWithComments>(`/api/item/comment/itemWithComment/${itemId}`),
+  const { data, error, isLoading, mutate } = useSWR(`/api/item/${itemId}`, () =>
+    fetcher<IItemWithComments>(`/api/item/${itemId}`),
   );
+
+  // Update data with new data once itemId is passed
+  useEffect(() => {
+    mutate();
+  }, [itemId, mutate]);
 
   return { item: data, error, isLoading, mutate };
 }
