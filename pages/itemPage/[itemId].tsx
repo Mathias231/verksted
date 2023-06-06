9cc4dace9447b08fa4f6410515116bf59779048e
Mathias231 <mathias.krogh@outlook.com>
2023-06-06 08:12:00 +0200

    Checking if user logged in

--- pages/itemPage/[itemId].tsx
+++ pages/itemPage/[itemId].tsx
@@ -1,13 +1,17 @@
 import SingleItemPage from '@/components/items/singleItemPage';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 function ItemPage() {
+  const { data: session } = useSession();
   const router = useRouter();
 
   // Gets itemId form query
   const { itemId } = router.query;
   if (typeof itemId !== 'string') return <div>Finner ikke gjenstand..</div>;
+
+  if (!session?.user) return <div>Du må logge inn for å se denne siden...</div>;
 
   return (
     <div className="container mx-auto sm:p-4">
